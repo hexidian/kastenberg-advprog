9e3f9e2c8dc10e23ddbb2a790428f25eab9ee4fc
student <student@US-Lab-18.local>
2017-09-19 11:26:05 -0400

    start first major work on merge_sort.c

--- C/merge_sort.c
+++ C/merge_sort.c
@@ -1,9 +1,42 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 #define True 1
 #define False 0
-int main (){
-  int listLength;
-  scanf("how long is this list?%d\n",listLength);
-  int startList[listLength];
-  return 1
+#define ListLength 20
+void PrintSimpleArray(int array[ListLength])
+{
+  int length = ListLength;
+  for (int i = 0; i<length; i++)
+  {
+    printf("%d , ",array[i]);
+  }
+  printf("\n");//this allows us to have all of the array elements on one line
+  return;
+}
+int merge_sort(int array[],int arrayLength)
+{
+  if (arrayLength == 1)
+  {
+    return array;
+  }
+  if (arrayLength == 2)
+  {
+    if (array[0] > array[1]) {return array}
+    int returningArray[2]; returningArray[0] = array[1]; returningArray[1] = array[0]; //just makes an array that is the reverse of the starting array
+    return returningArray;
+  }
+  //TODO split up the array and then combine the sorted version
+
+}
+int main ()
+{
+  srand(time(NULL));
+  int ElementRange = 40;
+  int startArray[ListLength];
+  for (int i = 0; i<ListLength; i++)
+  {
+    startArray[i] = rand() % 40;
+  }
+  return 1;
 }
