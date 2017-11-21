428d3f388938a338ae74abadfced90756f449e5d
hexidian <leokastenberg@gmail.com>
2017-11-21 12:28:42 -0500

    fix what I was messing up in uploading; also work

--- chessGame/chessGame.py
+++ chessGame/chessGame.py
@@ -21,6 +21,35 @@ class Board:
 
         return grid
 
+    def possiblePieceMoves(self,row,column):#row and column are the row and column of the piece that I am finding the possible moves of
+
+        pieceType = self.grid[row][column][0]
+        pieceColor = self.grid[row][column][1]
+
+        baseMove = [pieceColor,pieceType,row,column]
+
+        foundMoves = []
+
+        for rowTo in range(8):
+            for columnTo in range(8):
+                testingMove = baseMove[:]
+                testingMove.append(rowTo).append(columnTo)
+                if self.isLegalMove(testingMove):
+                    foundMoves.append(testingMove)
+
+        return foundMoves
+
+    def possibleColorMoves(self,color):
+        foundMoves = []
+
+        for row in range(8):
+            for column in range(8):
+                if self.grid[row][column][1]==color:
+                    foundMoves.extend(self.possiblePieceMoves(row,column))
+
+        return foundMoves
+
+
     def isLegalMove(self,move): #move is being passed in the format: ["color","type",row,column,rowTo,columnTo]
         pieceType = move[1]
 
@@ -33,22 +62,88 @@ class Board:
             return self.isLegalPawnMove(move)
 
         if pieceType == "N":
-            return self.isLegalKnightMove(move)#TODO
+            return self.isLegalKnightMove(move)
 
         if pieceType == "R":
-            return self.isLegalRookMove(move)#TODO
+            return self.isLegalRookMove(move)
 
         if pieceType == "K":
-            return self.isLegalKingMove(move)#TODO
+            return self.isLegalKingMove(move)
 
         if pieceType == "B":
-            return self.isLegalBishopMove(move)#TODO
+            return self.isLegalBishopMove(move)
 
         if pieceType == "Q":
-            return (self.isLegalBishopMove or self.isLegalRookMove)
+            return (self.isLegalBishopMove(move) or self.isLegalRookMove(move))
 
-    def isLegalBishopMove(self,move):
-        pass #TODO NEXT CLASS
+    def isLegalKingMove(self,move): #move is being passed in the format: ["color","type",row,column,rowTo,columnTo]
+        deltaRow = abs(move[2]-move[4])
+        deltaCol = abs(move[3]-move[5])
+        movementCorrect = False
+        movetoSafe = False
+        if (deltaRow==1 and (deltaCol==0 or deltaCol==1)) or (deltaCol==1 and (deltaRow==0 or deltaRow==1)):
+            movementCorrect = True
+        try:
+            if self.grid[move[4]][move[5]][1] == move[0]:
+                movetoSafe = False
+            else:
+                movetoSafe = True
+        except IndexError:
+            movetoSafe = True
+
+        return movetoSafe and movementCorrect
+
+    def isLegalKnightMove(self,move): #move is being passed in the format: ["color","type",row,column,rowTo,columnTo]
+        deltaRow,deltaCol = abs(move[2]-move[4]),abs(move[3]-move[5])
+        movementCorrect = False#they need to be false by default, if they should be true I will make them true
+        gotoSafe = False
+        if ((deltaRow==1 and deltaCol==2) or (deltaRow==2 and deltaCol==1)):
+            movementCorrect = True
+
+        try:
+            if (move[0] == "w") and (self.grid[move[4]][move[5]][0] == "b"):
+                gotoSafe = True
+            elif (move[0] == "b") and (self.grid[move[4]][move[5]][0] == "w"):
+                gotoSafe = True
+        except IndexError:
+            gotoSafe = True
+
+        return (movementCorrect and gotoSafe)
+
+
+    def isLegalBishopMove(self,move): #move is being passed in the format: ["color","type",row,column,rowTo,columnTo]
+        if not (abs(move[2]-move[4]) == abs(move[3]-move[5])):
+            return False
+
+
+        if move[2] < move[4]:
+            if move[3] < move[5]:
+                for row in range(move[2]+1,move[4]):
+                    for column in range(move[3]+1,move[5]):
+                        if self.grid[row][column] != "":
+                            return False
+            else:
+                for row in range(move[2]+1,move[4]):
+                    for column in range(move[5]+1,move[3]):
+                        if self.grid[row][column] != "":
+                            return False
+        else:
+            if move[3] < move[5]:
+                for row in range(move[4]+1,move[2]):
+                    for column in range(move[3]+1,move[5]):
+                        if self.grid[row][column] != "":
+                            return False
+            else:
+                for row in range(move[4]+1,move[2]):
+                    for column in range(move[5]+1,move[3]):
+                        if self.grid[row][column] != "":
+                            return False
+        try:
+            if self.grid[move[4]][move[5]][1] == move[0]: return False  #if there is a same-colored piece where you want to go
+        except:
+            pass
+
+        return True
 
     def isLegalPawnMove(self,move): #move is being passed in the format: ["color","type",row,column,rowTo,columnTo]
         if move[0]=="w":#moving as a white piece is opposite of moving as a black piece, so it will be programmed seperately
@@ -79,14 +174,24 @@ class Board:
         if not ((move[2]==move[4]) or (move[3]==move[5])): #makes sure that it is either the same row or column
             return False
         if move[2] == move[4]: #if we are doing a
-            if move[3] > move[5]:
-                for i in range (move[3]+1,move[5]+1):
+            if move[5] > move[3]:
+                for i in range (move[3]+1,move[5]):
                     if self.grid[move[2]][i] != "":    #if the path is blocked by another piece
                         return False
             else:
-                for i in range (move[2]+1,move[4]+1):
-                    if self.grid[move[3]][i] != "":#if the path is blocked by another piece
+                for i in range (move[5]+1,move[3]):
+                    if self.grid[move[2]][i] != "":    #if the path is blocked by another piece
                         return False
+                    #alsd
+
+        else:
+            if move[2]>move[4]:
+                for i in range(move[4]+1,move[2]):
+                    if self.grid[i][move[3]] != "": return False
+            else:
+                for i in range(move[2]+1,move[4]):
+                    if self.grid[i][move[3]] != "": return False
+
         try:
             if self.grid[move[4]][move[5]][1] == move[0]: return False  #if there is a same-colored piece where you want to go
         except:
@@ -94,22 +199,63 @@ class Board:
 
         return True
 
-    def printGrid(self):
-        for i in self.grid:
-            print i
+    def movePiece(self,move): #move is being passed in the format: ["color","type",row,column,rowTo,columnTo]
+
+        #NOTE only call this function if you have already checked if the move is a legal move
+
+        self.grid[move[2]][move[3]] = ""
+        self.grid[move[4]][move[5]] = move[1]+move[0]
+
+    def takePlayerMove(self,colorsTurn):
+        while 1: #because python doesn't have any heckin' do while loops
+            print "it is "+colorsTurn+"'s turn\n"
+            rawMove = raw_input("input move in the format:\nPieceAbbreviation (Knights are 'N') row column rowTo columnTo\n\n>>>").split() #currently in the format ['piece',row,column,rowTo,columnTo]
+
+            move = [colorsTurn]
+            for i in range(1,5):
+                rawMove[i] = int(rawMove[i])
+            move.extend(rawMove)
+            if self.isLegalMove(move):
+                break
+            else:
+                print "that move was not a legal move, please check how you formated it"
+
+        self.movePiece(move)
+
+    def printGrid(self): #temporary, not a very nice looking print; TODO make more nice looking
+        print "  " + str([(" "+str(i)+" ") for i in range(8)])+"\n"
+        for y in range(8):
+            row = self.grid[y]
+            print str(y)+" "+str([(row[i]+" " if len(row[i])>0 else "   ") for i in range(8)])+"\n"
 
 
 
 class Game():
 
-    def __init__(self,blackOrWhite):
-        self.board = Board
+    def __init__(self):
+        self.board = Board()
+
+        if input("how many players?")==1:
+            self.gameType = 1
+            self.blackOrWhite = raw_input("black or white? (b/w):")
+            self.playSinglePlayer()#TODO
+        else:
+            self.gameType = 2
+            self.playGame()
+
+    def isCheckmate(self): #TODO
+        return False
+
+    def playGame(self):
+        whoseTurn = "w"
+        while (not self.isCheckmate()):
+            self.board.printGrid()
+            self.board.takePlayerMove(whoseTurn)
+            whoseTurn = ("w" if whoseTurn == "b" else "b")
+
 
 def main():
-    board = Board()
-
-    board.printGrid()
+    game = Game()
 
 if __name__ == "__main__":
     main()
-    board = Board()
